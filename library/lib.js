7f6f7bbe12db3ad3bbd9929ca0902f70d5a64751
= <lukejmullins@gmail.com>
2025-10-20 12:04:37 -0400

    Add basic css and button

--- library/lib.js
+++ library/lib.js
@@ -8,7 +8,7 @@ function Book(title, pages, author, id) {
   }
   this.title = title;
   this.pages = pages;
-  this.authors = author;
+  this.author = author;
   this.id = id;
 }
 
@@ -20,12 +20,21 @@ function addBookToLibrary(title, pages, author) {
 
 addBookToLibrary("Dune", "630", "Frank Herbert");
 addBookToLibrary("LOTR", "1020", "JRR Tolkien");
+addBookToLibrary("Crime and Punishment", "407", "Fydor Dostovesky");
 console.log(myLibrary);
 
 function displayLibrary(arr) {
   arr.forEach(function (book) {
     let newBook = document.createElement("div");
-    newBook.textContent = book.title;
+    let title = document.createElement("h1");
+    let pages = document.createElement("p");
+    let author = document.createElement("p");
+    title.textContent = book.title;
+    pages.textContent = book.pages;
+    author.textContent = book.author;
+    newBook.appendChild(title);
+    newBook.appendChild(pages);
+    newBook.appendChild(author);
     newBook.classList.add("card");
     container.appendChild(newBook);
   });
